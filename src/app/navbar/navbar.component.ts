af8aa370b84271b2b7c6a50bcc99e18d4da07996
kleuber <kleuber@senado.leg.br>
2021-01-23 18:20:57 -0300

    Criação do projeto

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbDropdown } from '@ng-bootstrap/ng-bootstrap'
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  isCollapsed = true;
+  collapsed = true;
+
+  ngOnInit(): void {
+  }
+  
+  constructor() {}
+
+  toggleMenu() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+}
